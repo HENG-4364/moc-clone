ff4f4761eb859b281d5156a22f6bbe554e74693c
Lyheng <senglyheng2001@gmail.com>
2025-01-06 14:25:32 +0700

    update

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,22 +1,45 @@
 import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
+  // return {
+  //   name: "My Next.js PWA",
+  //   short_name: "NextPWA",
+  //   description: "A Progressive Web App built with Next.js",
+  //   start_url: "/",
+  //   display: "standalone",
+  //   background_color: "#ffffff",
+  //   theme_color: "#000000",
+  //   icons: [
+  //     {
+  //       src: "/icon-192x192.png",
+  //       sizes: "192x192",
+  //       type: "image/png",
+  //     },
+  //     {
+  //       src: "/icon-512x512.png",
+  //       sizes: "512x512",
+  //       type: "image/png",
+  //     },
+  //   ],
+  // };
   return {
-    name: "My Next.js PWA",
-    short_name: "NextPWA",
-    description: "A Progressive Web App built with Next.js",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
+    name: 'Ministry of Commerce',
+    lang: 'km',
+    display_override: ['standalone'],
+    short_name: 'Attendance',
+    description: 'Check your attendance with seamless QR code scanning',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#ffffff',
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/MoC-Icon.svg",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/MoC-Icon.svg",
         sizes: "512x512",
         type: "image/png",
       },
