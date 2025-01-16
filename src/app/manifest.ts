744f2144df0a730ee868b14caa1a2841cc42bbf8
Lyheng <senglyheng2001@gmail.com>
2025-01-16 16:31:26 +0700

    update

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,27 +1,6 @@
 import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-  // return {
-  //   name: "My Next.js PWA",
-  //   short_name: "NextPWA",
-  //   description: "A Progressive Web App built with Next.js",
-  //   start_url: "/",
-  //   display: "standalone",
-  //   background_color: "#ffffff",
-  //   theme_color: "#000000",
-  //   icons: [
-  //     {
-  //       src: "/icon-192x192.png",
-  //       sizes: "192x192",
-  //       type: "image/png",
-  //     },
-  //     {
-  //       src: "/icon-512x512.png",
-  //       sizes: "512x512",
-  //       type: "image/png",
-  //     },
-  //   ],
-  // };
   return {
     name: 'Ministry of Commerce',
     lang: 'km',
@@ -34,14 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#2980B9',
     icons: [
       {
-        src: "/MoC-Icon.svg",
-        sizes: "192x192",
-        type: "image/svg+xml",
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
-        src: "/MoC-Icon.svg",
-        sizes: "512x512",
-        type: "image/svg+xml",
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
