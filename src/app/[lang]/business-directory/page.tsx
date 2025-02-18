93fe1df74ef9a377194d8a77f1d2773099515317
Lyheng <senglyheng2001@gmail.com>
2025-02-18 16:33:53 +0700

    update

--- src/app/[lang]/business-directory/page.tsx
+++ src/app/[lang]/business-directory/page.tsx
@@ -1,12 +1,27 @@
 import BusinessDirectoryScreen from "@/screen/BusinessDirectory";
 import React from "react";
-
-const BusinessDirectoryPage = () => {
+import { getDictionaryByFolder } from "../dictionaries";
+import { Metadata } from "next";
+type Params = Promise<{ lang: string }>;
+const BusinessDirectoryPage = async ({ params }: { params: Params }) => {
+  const { lang } = await params;
+  const dict = await getDictionaryByFolder(lang, "business_directory");
   return (
     <>
-      <BusinessDirectoryScreen />
+      <BusinessDirectoryScreen dict={dict} />
     </>
   );
 };
 
 export default BusinessDirectoryPage;
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const { lang } = await params;
+  const dict = await getDictionaryByFolder(lang, "business_directory");
+  return {
+    title: dict?.head_title,
+  };
+}
