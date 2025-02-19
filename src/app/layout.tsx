455744d31aea479ad37c9bf136e7b4bb99c3d4f3
Lyheng <senglyheng2001@gmail.com>
2025-02-19 09:55:51 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,6 +58,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Attendance" />
+      </head>
       <body className={`${hanuman.className} ${moul.className}`}>
         {children}
         <PWAInstallPrompt />
